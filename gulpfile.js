15f63eb804269297325d75dc2c9ad4e696ca12fa
EugeneHorungy <77688070+EugeneHorungy@users.noreply.github.com>
2021-05-16 10:19:39 +0300

    test-bug-fix

--- gulpfile.js
+++ gulpfile.js
@@ -157,7 +157,8 @@ exports.reload = reload;
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
   gulp.watch("source/js/*.js", gulp.series(scripts));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/*.html", gulp.series(html, reload));
+  // gulp.watch("source/*.html").on("change", sync.reload);
 }
 
 // Build
